5878e168d05ee91b5b6a939ed1f50fd662f6d24b
Christoph Neuhauser <c.a.neuhauser@gmail.com>
2025-08-29 00:33:36 +0200

    Added unordered map test (2).

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,6 @@
 #include <unordered_map>
 #include <iostream>
+#include <string>
 #include <cstdint>
 
 // See: https://stackoverflow.com/questions/4948780/magic-number-in-boosthash-combine
@@ -12,6 +13,9 @@ inline void hash_combine(std::size_t& s, const T& v) {
 struct CommandPoolType {
     uint32_t queueFamilyIndex = 0xFFFFFFFF; // 0xFFFFFFFF encodes standard (graphics queue).
     uint32_t threadIndex = 0;
+    std::string sval = "ABC";
+    unsigned long ulval = 100;
+    
 
     bool operator==(const CommandPoolType& other) const {
         return queueFamilyIndex == other.queueFamilyIndex && other.threadIndex == threadIndex;
@@ -25,16 +29,18 @@ template<> struct hash<CommandPoolType> {
         std::size_t result = 0;
         hash_combine(result, s.queueFamilyIndex);
         hash_combine(result, s.threadIndex);
+        hash_combine(result, s.sval);
+        hash_combine(result, s.sval);
         return result;
     }
 };
 }
 
-size_t doSomething() {
+std::unordered_map<CommandPoolType, int> doSomething() {
     size_t s = 0;
     int v = 1000;
     hash_combine(s, v);
     std::unordered_map<CommandPoolType, int> commandPools;
     commandPools.insert({ CommandPoolType{}, 0});
-    return s;
+    return commandPools;
 }
